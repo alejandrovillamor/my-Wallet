8b871e5964699fd1232a947a4f0e2e37bcbbce40
alejandrovillamor <122881552+alejandrovillamor@users.noreply.github.com>
2023-01-26 10:48:47 +0100

    proyecto social11

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBJNYlV1K036dugYv66M2Aqe0nqOrVzRNc",
+    authDomain: "my-wallet-7b9ed.firebaseapp.com",
+    projectId: "my-wallet-7b9ed",
+    storageBucket: "my-wallet-7b9ed.appspot.com",
+    messagingSenderId: "400759002237",
+    appId: "1:400759002237:web:d55d449b55e70795920d14"
+  }
+  
+};
